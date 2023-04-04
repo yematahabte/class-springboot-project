b672c29a87193f82bc879c9e6d5c8097bea8a81d
yemata <yemata1279@gmail.com>
2023-04-04 12:45:01 +0300

    initial commit class springboot project

---
+++ src/main/java/com/act/seclassmtapi/SeClassMtApiApplication.java
@@ -0,0 +1,13 @@
+package com.act.seclassmtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeClassMtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeClassMtApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/act/seclassmtapi/SeClassMtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.act.seclassmtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeClassMtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
